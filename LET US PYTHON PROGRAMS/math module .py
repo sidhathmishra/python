0360e7cd796097c69fedcb1bc0586ccd796f6013
sidhathmishra <127026798+sidhathmishra@users.noreply.github.com>
2023-05-17 23:42:13 +0530

    Add files via upload

---
+++ LET US PYTHON PROGRAMS/math module .py
@@ -0,0 +1,11 @@
+import math
+x = 100
+print(math.factorial(x))#factorial of x 
+print(math.degrees(math.pi))#radians to degrees
+print(math.factorial(5))#factorial of x
+print(math.log(x))#natural log of x 
+
+
+
+
+
