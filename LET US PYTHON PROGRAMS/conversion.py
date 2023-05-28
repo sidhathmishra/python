646f3496b2702a71203e2c98bd5db90f99fb1989
sidhathmishra <127026798+sidhathmishra@users.noreply.github.com>
2023-05-28 18:09:26 +0530

    Add files via upload

---
+++ LET US PYTHON PROGRAMS/conversion.py
@@ -0,0 +1,28 @@
+# convert to int
+print(int(3.14))#converting to int
+a = int('485')#numeric string to int
+b = int('768')#numeric string to int
+c = a + b
+print(c)
+
+print(int('1011',2))#binary to decimal
+print(int('361',8))#octal to decimal
+print(int('21',16))#hex to decimal 
+
+#convert to float 
+print(float(35))#int to float
+i = float('4.85')#numeric string to float
+j = float('7.68')#numeric string to float
+print(i+j)
+
+#convert to complex 
+print(complex(35))#int to float
+x = complex(4.85,1.1)#string to float
+y = complex(7.68,2.1)#string to float
+print(x+y)
+
+#convert to bool
+print(bool(35))
+print(bool(1.2))
+print(int(True))
+print(int(False))
