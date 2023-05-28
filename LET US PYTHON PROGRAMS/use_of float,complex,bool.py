48f3d922cd36ee046721468b4b350af5f11de56f
sidhathmishra <127026798+sidhathmishra@users.noreply.github.com>
2023-05-28 18:10:25 +0530

    Add files via upload

---
+++ LET US PYTHON PROGRAMS/use_of float,complex,bool.py
@@ -0,0 +1,21 @@
+#use of float 
+i = 3.5
+j = 1.2
+print(i%j)
+
+#use of complex
+a = 1 +2j
+b = 3*(1+2j)
+c = a*b
+print(a)
+print(b)
+print(c)
+print(a.real)
+print(a.imag)
+print(a.conjugate)
+
+#use of bool
+x =True
+y = 3 > 4
+print(x)
+print(y) 
