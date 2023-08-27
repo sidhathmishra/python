58c5e3aa30a7efd7167f5c5c0922c90fb4987005
sidhathmishra <127026798+sidhathmishra@users.noreply.github.com>
2023-08-27 10:30:30 +0530

    Add files via upload

---
+++ LET US PYTHON PROGRAMS/ch 6/use of while loop 3.py
@@ -0,0 +1,7 @@
+s = 'Mumbai'
+lst = ['desert','dessert','to','too',' lose','loose']
+tpl = (10,20,30,-20,-10)
+i = 0
+while i < len(lst):
+    print(i,s[i],lst[i],tpl[i])
+    i += 1
---
+++ LET US PYTHON PROGRAMS/ch 6/use of while loop.py
@@ -0,0 +1,5 @@
+num = int(input("enter a number >>>"))
+while num != 5:
+    print(num,num*num)
+    num = int(input("enter a number >>"))
+###second code####
